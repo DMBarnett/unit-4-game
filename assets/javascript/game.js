e87f5b473d20d6ae56e7589cbafe1a5584492ff0
DMBarnettLaptop <davidmitchellbarnett@gmail.com>
2018-08-23 21:05:34 -0400

    Building the attack commands and creating the waves of enemies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     var gameStarted = false;
     var charSelectScreen = $("#charSelectScreen");
+    var gameScreen = $("#gameplayArea");
+    var yourCharacter;
+    var enemies = {
+        "foot":"Frozen Zombie",
+        "boss":"White Walker",
+        "king":"King White Walker"
+    }
+    var waveNumber = 1;
     class startingCharacter{
         constructor(name){
             this.level = 1;
@@ -13,14 +21,14 @@ $(document).ready(function(){
                 if(firstLetter(name)==="j"){
                     baseStrength = 10;
                 } else{
-                    baseStrength = 5;
+                    baseStrength = 2;
                 }
                 return baseStrength;
             })
             this.agility = ((name)=>{
                 var baseAgility = 1;
                 if(firstLetter(name) ==="j"){
-                    baseAgility = 5;
+                    baseAgility = 2;
                 } else{
                     baseAgility = 10;
                 }
@@ -30,16 +38,102 @@ $(document).ready(function(){
         attack(target){
             this.target = target;
             this.damage = 1+(Math.floor(Math.random()*(this.attack+this.strength))*this.agility);
-            target.baseHealth-=this.damage;
+            target.health-=this.damage;
         }
 
     }
+    class enemies{
+        constructor(type){
+            this.name = enemies.type;
+            this.health = ((type)=>{
+                if(type === "foot"){
+                    return 100;
+                }else if(type === "boss"){
+                    return 300;
+                }else{
+                    return 500;
+                }
+            })
+            this.attack = ((type)=>{
+                if(type === "foot"){
+                    return 20;
+                }else if(type === "boss"){
+                    return 30;
+                }else{
+                    return 40;
+                }
+            })
+            this.givenExp = ((type)=>{
+                if(type === "foot"){
+                    return (Math.floor(Math.random()*(50-20+1)+20));
+                }else if(type === "boss"){
+                    return (Math.floor(Math.random()*(100-50+1)+20));;
+                }else{
+                    return 500;
+                }
+            })
+        }
+        slash(target){
+            this.target = target;
+            this.damage = 1+(Math.floor(Math.random()*this.attack));
+            target.health -= this.damage
+        }
+    }
+
+    function gainExp(target, yourCharacter){
+        yourCharacter.exp += target.givenExp;
+        if(yourCharacter.exp > yourCharacter.level*100){
+            yourCharacter.strength +=(Math.floor(rand.random()*3)+1);
+            yourCharacter.agility += (Math.floor(rand.random()*3)+1);
+            yourCharacter.health = 100+(50*yourCharacter.level);
+            yourCharacter.level++;
+            yourCharacter.exp -= yourCharacter.level*100;
+        }
+    }
+
+    function combat(target, yourCharacter){
+        yourCharacter.attack(target);
+        target.slash(yourCharacter);
+        if(yourCharacter.health < 0){
+            gameScreen.hide();
+            deathScreen.show();
+            return;
+        }
+        if(target.health <0){
+            gainExp(target, yourCharacter);
+            target.hide();
+        }
+    }
+
+    function potion(){
+        yourCharacter.health+=50;
+    };
+
+    function startGame(character){
+        for (let i = 0; i < waveNumber; i++) {
+            const element = array[i];
+        }
+    }
+
+    function createWave(iteration){
+
+    }
     $(".charSelect").on("click", function(){
-        startingCharacter(this.id);
+        yourCharacter = new startingCharacter(this.id);
         charSelectScreen.hide();
-        startGame(startingCharacter);
+        gameScreen.show();
+        startGame(yourCharacter);
     })
     
+    $(".target").on("click",function(){
+        combat(this.id, yourCharacter);
+    })
+
+    $("#potion").on("click", function(){
+        potion(this.id);
+        this.hide();
+    })
+
     function firstLetter(wordHere){
         return (wordHere.charAt(0).toLowerCase());
     }
